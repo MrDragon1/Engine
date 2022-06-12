e5440407a8ef4f14dd831e28265873e211d0a327
Mrdragon1 <1071314643@qq.com>
2022-06-12 22:37:45 +0800

    now use the proper toolchain

--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,6 @@ add_rules("mode.debug", "mode.release")
 add_requires("imgui v1.87-docking", {configs = {glfw_opengl3 = true}})
 add_requires("spdlog","glad","glfw","glm") -- glad must before glfw
 
-if (is_plat("linux")) then
-   set_toolchains("clang")
-end
 
 set_languages("c++20")
 set_optimize("fastest")
@@ -15,6 +12,7 @@ add_packages("imgui")
 add_packages("spdlog","glad","glfw","glm")
 
 target("Engine")
+    set_toolchains("clang")
     set_kind("static")
     set_pcxxheader("Engine/src/pch.hpp")
     add_files("Engine/src/**.cpp","Engine/vendor/**.cpp")
@@ -22,12 +20,14 @@ target("Engine")
 
     
 target("Sandbox")
+    set_toolchains("clang")
     add_deps("Engine")
     set_kind("binary")
     add_files("Sandbox/src/**.cpp")
     add_includedirs("Engine/src")
 
 target("Editor")
+    set_toolchains("clang")
     add_deps("Engine")
     set_kind("binary")
     add_files("Editor/src/**.cpp")
