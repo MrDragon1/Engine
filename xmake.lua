0acb2de5ff080e40336a55b9fd7072d1ed2d1056
Mrdragon1 <1071314643@qq.com>
2022-07-01 09:27:20 +0800

    Set default target

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ add_requires("imgui v1.87-docking", {configs = {glfw_opengl3 = true}})
 add_requires("spdlog","glad","glfw","glm","entt","yaml-cpp") -- glad must before glfw
 
 
-set_languages("c++20")
+set_languages("cxx20")
 set_optimize("fastest")
 add_defines("ENGINE_PLATFORM_LINUX","ENGINE_ENABLE_ASSERTS")
 
@@ -12,22 +12,26 @@ add_packages("imgui")
 add_packages("spdlog","glad","glfw","glm","entt","yaml-cpp")
 
 target("Editor")
+    set_default(true)
     set_toolchains("clang")
+    set_rundir("$(projectdir)/Editor")
     add_deps("Engine")
     set_kind("binary")
     add_files("Editor/src/**.cpp")
     add_includedirs("Engine/src","Engine/vendor")
 
+
 target("Engine")
     set_toolchains("clang")
     set_kind("static")
     set_pcxxheader("Engine/src/pch.hpp")
-    add_files("Engine/src/**.cpp","Engine/vendor/**.cpp")
-    add_syslinks("comdlg32")
-    add_includedirs("Engine/src","Engine/vendor")
 
-    
+    add_files("Engine/src/**.cpp","Engine/vendor/**.cpp")
+    add_includedirs("Engine/src","Engine/vendor")
+    add_syslinks("comdlg32")
+
 target("Sandbox")
+    set_default(false)
     set_toolchains("clang")
     add_deps("Engine")
     set_kind("binary")
