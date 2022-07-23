f317141ff6e1ef9371efd034d0fbdd6101ab047d
Mrdragon1 <1071314643@qq.com>
2022-07-23 20:26:38 +0800

    Use ASSIMP to load model

--- Ethereal/src/Ethereal/Utils/AssetLoader.cpp
+++ Ethereal/src/Ethereal/Utils/AssetLoader.cpp
@@ -1,141 +1,102 @@
 #include "pch.hpp"
 #include "AssetLoader.hpp"
-#define TINYOBJLOADER_IMPLEMENTATION
-
 #include "stb_image/stb_image.h"
 
 namespace Ethereal
 {
+    std::vector<MeshVertex> ObjLoader::m_MeshVertices;
+    std::vector<unsigned int> ObjLoader::m_MeshIndices;
+
+    void ObjLoader::processMesh(aiMesh* mesh, const aiScene* scene) {
+        // walk through each of the mesh's vertices
+        for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            MeshVertex vertex;
+            glm::vec3 vector;  // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3
+                               // class so we transfer the data to this placeholder glm::vec3 first.
+            // positions
+            vector.x = mesh->mVertices[i].x;
+            vector.y = mesh->mVertices[i].y;
+            vector.z = mesh->mVertices[i].z;
+            vertex.Position = vector;
+            // normals
+            if (mesh->HasNormals()) {
+                vector.x = mesh->mNormals[i].x;
+                vector.y = mesh->mNormals[i].y;
+                vector.z = mesh->mNormals[i].z;
+                vertex.Normal = vector;
+            }
+            // texture coordinates
+            if (mesh->mTextureCoords[0])  // does the mesh contain texture coordinates?
+            {
+                glm::vec2 vec;
+                // a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't
+                // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
+                vec.x = mesh->mTextureCoords[0][i].x;
+                vec.y = mesh->mTextureCoords[0][i].y;
+                vertex.UV = vec;
+                // // tangent
+                // vector.x = mesh->mTangents[i].x;
+                // vector.y = mesh->mTangents[i].y;
+                // vector.z = mesh->mTangents[i].z;
+                // vertex.Tangent = vector;
+                // // bitangent
+                // vector.x = mesh->mBitangents[i].x;
+                // vector.y = mesh->mBitangents[i].y;
+                // vector.z = mesh->mBitangents[i].z;
+                // vertex.Bitangent = vector;
+            } else
+                vertex.UV = glm::vec2(0.0f, 0.0f);
+
+            m_MeshVertices.push_back(vertex);
+        }
+
+        // now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
+        for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+            aiFace face = mesh->mFaces[i];
+            // retrieve all indices of the face and store them in the indices vector
+            for (unsigned int j = 0; j < face.mNumIndices; j++) m_MeshIndices.push_back(face.mIndices[j]);
+        }
+    }
+
+    void ObjLoader::processNode(aiNode* node, const aiScene* scene) {
+        // 处理节点所有的网格（如果有的话）
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            processMesh(mesh, scene);
+        }
+        // 接下来对它的子节点重复这一过程
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            processNode(node->mChildren[i], scene);
+        }
+    }
     void ObjLoader::Load(const GameObjectMeshDesc& desc, RenderMeshData& renderMeshData) {
-        tinyobj::ObjReaderConfig reader_config;
-        tinyobj::ObjReader* reader = new tinyobj::ObjReader();
+        m_MeshVertices.clear();
+        m_MeshIndices.clear();
+        Assimp::Importer importer;
 
         std::string inputfile = desc.m_filePath;
-        if (!reader->ParseFromFile(inputfile, reader_config)) {
-            if (!reader->Error().empty()) {
-                ET_CORE_ERROR("ObjLoader::Load Error: {0} when load {1}", reader->Error(), inputfile);
-            }
-            exit(1);
+        const aiScene* scene = importer.ReadFile(inputfile, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            ET_CORE_ERROR("ObjLoader::Load Error: {0} when load {1}", importer.GetErrorString(), inputfile);
+            return;
         }
 
-        if (!reader->Warning().empty()) {
-            ET_CORE_WARN("ObjLoader: {}", reader->Warning());
+        processNode(scene->mRootNode, scene);
+
+        renderMeshData.m_static_mesh_data.m_vertex_buffer = CreateRef<BufferData<MeshVertex>>(m_MeshVertices.size());
+        renderMeshData.m_static_mesh_data.m_index_buffer = CreateRef<BufferData<uint32_t>>(m_MeshIndices.size());
+
+        assert(m_MeshVertices.size() <= std::numeric_limits<uint32_t>::max());  // take care of the index range, should be
+                                                                                // consistent with the index range used by
+                                                                                // vulkan
+        uint32_t* indices = (uint32_t*)renderMeshData.m_static_mesh_data.m_index_buffer->m_data;
+        for (size_t i = 0; i < m_MeshVertices.size(); i++) {
+            ((MeshVertex*)(renderMeshData.m_static_mesh_data.m_vertex_buffer->m_data))[i] = m_MeshVertices[i];
+            // indices[i] = static_cast<uint32_t>(i);
         }
-        auto& attrib = reader->GetAttrib();
-        auto& shapes = reader->GetShapes();
-        auto& materials = reader->GetMaterials();
-
-        std::vector<MeshVertex> mesh_vertices;
-
-        // Loop over shapes
-        for (size_t s = 0; s < shapes.size(); s++) {
-            // TODO: support multiple materials for multiple shapes in one obj file
-            if (s > 1) ET_CORE_WARN("Ethereal only support one shape, because only one material per object is supported");
-            // Loop over faces(polygon)
-            size_t index_offset = 0;
-            for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-                size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
-                size_t material_id = shapes[s].mesh.material_ids[f];
-
-                if (fv != 3) {
-                    ET_CORE_WARN("ObjLoader: Only triangle is supported");
-                    continue;
-                }
-
-                glm::vec3 vertex[3];
-                glm::vec3 normal[3];
-                glm::vec2 uv[3];
-
-                bool has_normal = true;
-                bool has_uv = true;
-                // Loop over vertices in the face.
-                for (size_t v = 0; v < fv; v++) {
-                    // access to vertex
-                    tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-                    tinyobj::real_t vx = attrib.vertices[3 * size_t(idx.vertex_index) + 0];
-                    tinyobj::real_t vy = attrib.vertices[3 * size_t(idx.vertex_index) + 1];
-                    tinyobj::real_t vz = attrib.vertices[3 * size_t(idx.vertex_index) + 2];
-                    vertex[v] = glm::vec3(vx, vy, vz);
-                    // Check if `normal_index` is zero or positive. negative = no normal data
-                    if (idx.normal_index >= 0) {
-                        tinyobj::real_t nx = attrib.normals[3 * size_t(idx.normal_index) + 0];
-                        tinyobj::real_t ny = attrib.normals[3 * size_t(idx.normal_index) + 1];
-                        tinyobj::real_t nz = attrib.normals[3 * size_t(idx.normal_index) + 2];
-                        normal[v] = glm::vec3(nx, ny, nz);
-                    } else
-                        has_normal = false;
-
-                    // Check if `texcoord_index` is zero or positive. negative = no texcoord data
-                    if (idx.texcoord_index >= 0) {
-                        tinyobj::real_t tx = attrib.texcoords[2 * size_t(idx.texcoord_index) + 0];
-                        tinyobj::real_t ty = attrib.texcoords[2 * size_t(idx.texcoord_index) + 1];
-                        uv[v] = glm::vec2(tx, ty);
-                    } else
-                        has_uv = false;
-
-                    // ET_CORE_INFO("diffuse {0} {1} {2}",materials[material_id].diffuse[0], materials[material_id].diffuse[1],
-                    // materials[material_id].diffuse[2]);
-                    //  Optional: vertex colors
-                    //  tinyobj::real_t red = attrib.colors[3 * size_t(idx.vertex_index) + 0];
-                    //  tinyobj::real_t green = attrib.colors[3 * size_t(idx.vertex_index) + 1];
-                    //  tinyobj::real_t blue = attrib.colors[3 * size_t(idx.vertex_index) + 2];
-                }
-                index_offset += fv;
-
-                if (!has_normal) {
-                    glm::vec3 v0 = vertex[1] - vertex[0];
-                    glm::vec3 v1 = vertex[2] - vertex[1];
-                    normal[0] = glm::normalize(glm::cross(v0, v1));
-                    normal[1] = normal[0];
-                    normal[2] = normal[0];
-                }
-
-                if (!has_uv) {
-                    uv[0] = glm::vec2(0.5f, 0.5f);
-                    uv[1] = glm::vec2(0.5f, 0.5f);
-                    uv[2] = glm::vec2(0.5f, 0.5f);
-                }
-
-                glm::vec3 tangent{1, 0, 0};
-                {
-                    glm::vec3 edge1 = vertex[1] - vertex[0];
-                    glm::vec3 edge2 = vertex[2] - vertex[1];
-                    glm::vec2 deltaUV1 = uv[1] - uv[0];
-                    glm::vec2 deltaUV2 = uv[2] - uv[1];
-
-                    auto divide = deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y;
-                    if (divide >= 0.0f && divide < 0.000001f)
-                        divide = 0.000001f;
-                    else if (divide < 0.0f && divide > -0.000001f)
-                        divide = -0.000001f;
-
-                    float df = 1.0f / divide;
-                    tangent.x = df * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-                    tangent.y = df * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-                    tangent.z = df * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-                    tangent = glm::normalize(tangent);
-                }
-
-                for (size_t i = 0; i < 3; i++) {
-                    MeshVertex mesh_vert{};
-                    mesh_vert.Position = vertex[i] / glm::vec3(1);
-                    mesh_vert.Normal = normal[i];
-                    mesh_vert.UV = uv[i];
-                    mesh_vertices.push_back(mesh_vert);
-                }
-
-                renderMeshData.m_static_mesh_data.m_vertex_buffer = CreateRef<BufferData<MeshVertex>>(mesh_vertices.size());
-                renderMeshData.m_static_mesh_data.m_index_buffer = CreateRef<BufferData<uint32_t>>(mesh_vertices.size());
-
-                assert(mesh_vertices.size() <= std::numeric_limits<uint32_t>::max());  // take care of the index range, should be
-                                                                                       // consistent with the index range used by
-                                                                                       // vulkan
-                uint32_t* indices = (uint32_t*)renderMeshData.m_static_mesh_data.m_index_buffer->m_data;
-                for (size_t i = 0; i < mesh_vertices.size(); i++) {
-                    ((MeshVertex*)(renderMeshData.m_static_mesh_data.m_vertex_buffer->m_data))[i] = mesh_vertices[i];
-                    indices[i] = static_cast<uint32_t>(i);
-                }
-            }
+        for(size_t i = 0; i < m_MeshIndices.size(); i++) {
+            indices[i] = m_MeshIndices[i];
         }
     }
 
--- Ethereal/src/Ethereal/Utils/AssetLoader.hpp
+++ Ethereal/src/Ethereal/Utils/AssetLoader.hpp
@@ -1,21 +1,28 @@
 #pragma once
 
-// #define TINYOBJLOADER_IMPLEMENTATION  // define this in only *one* .cc
-// Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust trinagulation. Requires C++11
-//#define TINYOBJLOADER_USE_MAPBOX_EARCUT
-#include "tiny_obj_loader.h"
-
 #include "Ethereal/Renderer/Common.hpp"
 #include "Ethereal/Core/Base/Singleton.hpp"
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 namespace Ethereal
 {
-
-    class ObjLoader : public Singleton<ObjLoader>{
+    class ObjLoader : public Singleton<ObjLoader> {
       public:
         static void Load(const GameObjectMeshDesc& desc, RenderMeshData& renderMeshData);
+
+      private:
+        static std::vector<MeshVertex> m_MeshVertices;
+        static std::vector<unsigned int> m_MeshIndices;
+
+      private:
+        static void processNode(aiNode* node, const aiScene* scene);
+        static void processMesh(aiMesh* mesh, const aiScene* scene);
     };
 
-    class TextureLoader : public Singleton<TextureLoader>{
+    class TextureLoader : public Singleton<TextureLoader> {
       public:
         static void LoadPath(const std::string& path, Ref<TextureData>& textureData, bool flip = true);
     };
